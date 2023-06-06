7779f55e8b9a43ad7d0e5c83b6d99f4b30d9b98a
Victor Paul <Paulvictoroluwasegun@gmail.com>
2023-06-06 01:13:23 +0100

    EDITED

--- js/apps.js
+++ js/apps.js
@@ -1,5 +1,20 @@
 const mobileMenu = document.querySelector(".mobile_nav");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#number");
 
 function dropMenu() {
   mobileMenu.classList.toggle("show_mobile_nav");
 }
+
+function SubmitBtn() {
+  alert(`you have succesfully created an account, ENJOY!`);
+}
+
+// function SubmitBtn() {
+//   if (name == "Victor" && email == "ganad@gmail.com" && phone == 0122334455) {
+//     alert("You have succefully booked an appointment. Thank you!");
+//   } else {
+//     ("Please check your details again");
+//   }
+// }
